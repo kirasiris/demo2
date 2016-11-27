4153bd291994cc09c99a1ebf3bfd66f1716ab82b
kirasiris <kebin1421@hotmail.com>
2016-11-27 01:33:33 -0600

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,73 @@
+<section class="bp-footer bp-section bp-section-md-padding" id="contacts3-2b" style="background-color: rgb(46, 46, 46); padding-top: 90px; padding-bottom: 90px;">
+
+
+    <div class="row">
+    
+            <div class="bp-company col-xs-12 col-md-6 col-lg-3">
+
+                <div class="bp-company card">
+                    <div><img src="assets/images/logo 1.png" class="card-img-top"></div>
+                    <div class="card-block">
+                        <p class="card-text">Footer with solid color background and a contact form, Easily add subscribe and contact forms without any server-side integration.</p>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">
+                            <span class="list-group-icon"><span class="etl-icon icon-phone bp-iconfont-company-contacts3"></span></span>
+                            <span class="list-group-text">+1 (0) 000 0000 001 or +1 (0) 000 0000 002</span>
+                        </li>
+                        <li class="list-group-item">
+                            <span class="list-group-icon"><span class="etl-icon icon-map-pin bp-iconfont-company-contacts3"></span></span>
+                            <span class="list-group-text">1234 Street Name, City AA 99999</span>
+                        </li>
+                        <li class="list-group-item active">
+                            <span class="list-group-icon"><span class="etl-icon icon-envelope bp-iconfont-company-contacts3"></span></span>
+                            <span class="list-group-text"><a href="mailto:http://blogpersonal.net/" style="color:#F1C050;">http://blogpersonal.net/</a></span>
+                        </li>
+                    </ul>
+                </div>
+
+            </div>
+            <div class="bp-footer-content col-xs-12 col-md-6 col-lg-3">
+                <h4>Categories</h4>
+                <ul><li><a class="text-white" href="#">Business</a></li><li><a class="text-white" href="#">Design</a></li><li><a class="text-white" href="#">Real life</a></li><li><a class="text-white" href="#">Science</a></li><li><a class="text-white" href="#">Tech</a></li></ul>
+            </div>
+            <div class="bp-footer-content col-xs-12 col-md-6 col-lg-3">
+                <p><strong>Contacts</strong><br>Email: http://blogpersonal.net/<br>Phone: +1 (0) 000 0000 001<br>Fax: +1 (0) 000 0000 002<br><br><br><strong>Address</strong><br>1234 Street Name<br>City, AA 99999</p>
+            </div>
+            <div class="col-xs-12 col-md-6 col-lg-3" data-form-type="formoid">
+
+                <div data-form-alert="true">
+                    <div hidden="" data-form-alert-success="true">Thanks for filling out form!</div>
+                </div>
+
+                <form action="http://blogpersonal.net//" method="post" data-form-title="MESSAGE">
+
+                    <input type="hidden" value="JOs96j2KWH68yhj8YeGgjbkJO26fVKV+HdGKdtnSfjW7jglTWmC4uhkkLMTJWLgMv4yQTBNpW4lV/AioKOYfyzkIe6kXnTcpaUEQk14js0m3knPLGvmiyrft3bTuk7WA" data-form-email="true">
+
+                    
+
+                    <div class="form-group">
+                        <label class="form-control-label" for="contacts3-2b-email">Email<span class="form-asterisk">*</span></label>
+                        <input type="email" class="form-control input-sm input-inverse" name="email" required="" data-form-field="Email" id="contacts3-2b-email">
+                    </div>
+
+                    
+
+                    <div class="form-group">
+                        <label class="form-control-label" for="contacts3-2b-message">Message</label>
+                        <textarea class="form-control input-sm input-inverse" name="message" data-form-field="Message" rows="5" id="contacts3-2b-message"></textarea>
+                    </div>
+
+                    <div><button type="submit" class="btn btn-sm btn-black">Contact us</button></div>
+
+                </form>
+
+            </div>
+        </div>
+</section>
+<footer class="bp-small-footer bp-section bp-section-nopadding" id="footer1-38" style="background-color: rgb(50, 50, 50); padding-top: 1.75rem; padding-bottom: 1.75rem;">
+    
+    <div class="container">
+        <p class="text-xs-center">Business by <a href="https://blogpersonal.net/" style="color:#F1C050;">Blog Personal.</a></p>
+    </div>
+</footer>
---
+++ includes/nav.php
@@ -0,0 +1,37 @@
+<section id="ext_menu-2k">
+
+    <nav class="navbar navbar-dropdown navbar-fixed-top">
+        <div class="container">
+
+            <div class="bp-table">
+                <div class="bp-table-cell">
+
+                    <div class="navbar-brand">
+                        <a href="https://blogpersonal.net" class="navbar-logo"><img src="assets/images/logo 1.png" alt="Business"></a>
+                        
+                    </div>
+
+                </div>
+                <div class="bp-table-cell">
+
+                    <button class="navbar-toggler pull-xs-right hidden-md-up" type="button" data-toggle="collapse" data-target="#exCollapsingNavbar">
+                        <div class="hamburger-icon"></div>
+                    </button>
+
+                    <ul class="nav-dropdown collapse pull-xs-right nav navbar-nav navbar-toggleable-sm" id="exCollapsingNavbar">
+                    <li class="nav-item"><a class="nav-link link" href="index.php">HOME</a></li>
+                    <li class="nav-item"><a class="nav-link link" href="about.php">ABOUT US</a></li>
+                    <li class="nav-item"><a class="nav-link link" href="portfolio.php">PORTFOLIO</a></li>
+                    <li class="nav-item"><a class="nav-link link" href="contact.php">CONTACT</a></li>
+                    </ul>
+                    <button hidden="" class="navbar-toggler navbar-close" type="button" data-toggle="collapse" data-target="#exCollapsingNavbar">
+                        <div class="close-icon"></div>
+                    </button>
+
+                </div>
+            </div>
+
+        </div>
+    </nav>
+
+</section>
